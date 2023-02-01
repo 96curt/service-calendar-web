7d03f428cead56bc712468bd10f8c1b720620248
96curt <96curt@gmail.com>
2023-01-31 18:05:30 -0800

    markers

--- src/app/pages/tech-scheduling/map/map.component.ts
+++ src/app/pages/tech-scheduling/map/map.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MapSettings, MapSettingsService } from 'app/shared/services/map-settings.service';
+import CustomStore from 'devextreme/data/custom_store';
 import { environment } from 'environments/environment';
+import { Schedule, ServiceService } from 'openapi';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -10,8 +13,17 @@ import { environment } from 'environments/environment';
 export class MapComponent {
   keys = {bing:environment.bingKey};
   mapTypes = this.mapSettings.getMapTypes();
+  @Input() scheduleResource:CustomStore = new CustomStore;
+  markers: Schedule[] = [];
   constructor(
-    private mapSettings: MapSettingsService
-  ) {}
+    private mapSettings: MapSettingsService,
+    private serviceService: ServiceService
+  ) {
+    lastValueFrom(this.serviceService.serviceSchedulesList({}))
+    .then( (value) => {
+      this.markers = value;
+    })
+  }
+
 
 }
